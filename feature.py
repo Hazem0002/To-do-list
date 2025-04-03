1ab42402b9b95eabc1bc7c56e92255ca7afc8bf4
Hazem0002 <hazem.mo3taz11@gmail.com>
2025-04-03 20:35:56 +0200

    "Added feature to delete tasks from the to-do list."

---
+++ feature.py
@@ -0,0 +1,6 @@
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        deleted_task = tasks.pop(index)
+        print(f"Task deleted: {deleted_task}")
+    else:
+        print("Invalid task number.")
